8269099f89f5c4e5c0ca48c7a96af9b4fffb48ac
Shannon Miko <smiko@ups.com>
2019-03-13 16:21:53 -0400

    Added methods for multiplication and division

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -8,4 +8,12 @@ public class SimpleCalculator {
 	public int subtract(int a, int b) {
 		return a - b;
 	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public double divide(int a, int b) {
+		return Math.round((a / b) * 100) / 100.0;
+	}
 }// end of SimpleCalculator class
